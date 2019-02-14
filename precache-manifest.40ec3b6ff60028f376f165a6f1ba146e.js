e9f8c86a31f5106a36536eb530e252bcfded0308
cluntsao <tsaochelun@gmail.com>
2019-02-14 15:43:21 -0600

    Updates

---
+++ precache-manifest.40ec3b6ff60028f376f165a6f1ba146e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e18e5c6fb4628009cb6",
+    "url": "/people/static/js/runtime~main.1e18e5c6.js"
+  },
+  {
+    "revision": "43049b52147575713ed7",
+    "url": "/people/static/js/main.43049b52.chunk.js"
+  },
+  {
+    "revision": "71cb20f431c19c5e25cc",
+    "url": "/people/static/js/1.71cb20f4.chunk.js"
+  },
+  {
+    "revision": "43049b52147575713ed7",
+    "url": "/people/static/css/main.2bc49f2c.chunk.css"
+  },
+  {
+    "revision": "71cb20f431c19c5e25cc",
+    "url": "/people/static/css/1.09468764.chunk.css"
+  },
+  {
+    "revision": "738ffa35d34f105ab216a1e18bd517ad",
+    "url": "/people/index.html"
+  }
+];
